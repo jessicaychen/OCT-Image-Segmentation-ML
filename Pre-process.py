1f7ac94b424b3b9223cb22bd77a186b91b30ec1b
Jessica Chen <50808602+jessicaychen@users.noreply.github.com>
2020-02-07 00:11:11 -0800

    Add files via upload
    resize to 256x256 px and grayscale

---
+++ Pre-process.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 16:01:38 2020
+
+@author: JessicaChen
+"""
+
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
+
+import os
+from PIL import Image
+#import numpy as np
+#import cv2 
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
+
+
+save_dir = r'/Users/JessicaChen/Desktop/ML Data/Processed/' 
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+
+path = "/Users/JessicaChen/Desktop/ML Data/Original cSLOs/"
+dirs = os.listdir(path)
+
+# resize images to 256x256
+def process():
+    for item in dirs:
+        if not item.startswith('.')  and os.path.isfile(os.path.join(path, item)):
+             im = Image.open(path + item).convert("L")         #grayscale
+             f = os.path.splitext(item)[0]
+             new_dir = str(save_dir) + str(f)
+             imResize = im.resize((256,256), Image.ANTIALIAS)  #resize
+             imResize.save(new_dir + '.jpg', 'JPEG', quality=90)
+process()
+
+
+
+# Check
+#import matplotlib.pyplot as plt
+
+#img = plt.imread('/Users/JessicaChen/Desktop/ML Data/Processed/cSLO00.jpg')
+#plt.imshow(img,cmap = "gray")
+#print(img.shape) # (256,256) - 1 channel/grayscaled
+
+
+
+
